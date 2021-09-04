e20e25666eb77d67c6955db06473cd8da7c59b00
Zhou <bojing.zhou@student.unimelb.edu.au>
2021-09-04 19:15:50 +0800

    global styles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import PageTwo from './pages/PageTwo';
 import PageThree from './pages/PageThree';
 import PageFour from './pages/PageFour';
 import { withRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { createTheme } from '@material-ui/core';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#224878',
+    },
+    secondary: {
+      main: '#2196f3',
+    },
+  }
+});
 
 
 function App() {
   return (
     <div className={styles.App}>
+      <ThemeProvider theme={theme}>
       <Switch>
         <Route path="/" component={HomePage} exact />
         <Route path="/page1" component={PageOne} />
@@ -17,6 +31,7 @@ function App() {
         <Route path="/page3" component={PageThree} />
         <Route path="/page4" component={PageFour} />
       </Switch>
+      </ThemeProvider>
       <div>this is app</div>
     </div>
   );
