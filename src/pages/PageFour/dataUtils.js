1066139e617fc7ad948835cbd266beb982bdc2c7
zhangshiqi03 <zhangshiqi03@kuaishou.com>
2021-09-06 11:35:37 +0800

    add block world visualization

--- src/pages/PageFour/dataUtils.js
+++ src/pages/PageFour/dataUtils.js
@@ -1,20 +1,45 @@
-import d from './test.js';
+import d from './blockWorld.js';
 
-//console.log(d);
+function getAllBlocks() {
+    const stages = d.visualStages;
 
-function getInitBlocks() {
-    return d.visualStages[0].visualSprites.filter(s => s.prefabimage === "img-block");
+    const blocks = stages.map(stage => {
+        return stage.visualSprites.filter(s => s.prefabimage === "img-block");
+    })
+
+    return blocks;
 }
 
 function getClaw() {
     return d.visualStages[0].visualSprites.filter(s => s.prefabimage === "img-claw");
 }
 
+function getSteps() {
+    return d.visualStages.map((s =>s.stageName));
+}
 
-export const initBlocks = getInitBlocks();
+function getStepInfo() {
+    return d.visualStages.map((s =>s.stageInfo));
+}
+
+function getSubGoal() {
+    let map = new Map();
+    const subgoal = d.subgoalMap.m_values;
+    const step = d.subgoalMap.m_keys;
+    subgoal.map((subgoalList, i) => {
+        const currentStep = subgoalList[subgoalList.length-1];
+        if (!map.has(currentStep)) {
+            map.set(currentStep, step.slice(i))
+        }
+    })
+    return map;
+}
+export const allBlocks = getAllBlocks();
 export const claw = getClaw();
+export const steps = getSteps();
+export const stepInfo =  getStepInfo();
+export const subGoal = getSubGoal();
 
-
-export default null;
+//export default null;
 
 
--- src/pages/PageFour/index.js
+++ src/pages/PageFour/index.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Stage, Container, Sprite, Text,useTick,Graphics } from '@inlet/react-pixi';
-import { utils } from 'pixi.js';
-import {initBlocks, claw} from './dataUtils'
-const canvasWidth = 800
-const canvasHeight = 920
+import {Stage, Container, Sprite, Text, useTick, Graphics} from '@inlet/react-pixi';
+import {utils} from 'pixi.js';
+import {subGoal, stepInfo, allBlocks, claw, steps} from './dataUtils';
+
+import styles from './index.less';
+
+const canvasWidth_Middle = 800
+const canvasHeight_Middle = 800
+const canvasWidth_Left = 400
+const canvasHeight_Left = 800
+
+console.info('subGoal', [...subGoal.keys()]);
+
 class PageFour extends React.Component {
     // init data
     constructor(props) {
@@ -11,6 +19,8 @@ class PageFour extends React.Component {
 
         this.state = {
             // data that will be used/changed in render function
+            blockIndex: 0,
+            stepInfoIndex: 0
         }
         // Every function that interfaces with UI and data used
         // in this class needs to bind like this:
@@ -24,67 +34,107 @@ class PageFour extends React.Component {
     componentDidMount() {
     }
 
-    render() {
-        return (
-            <div>
-                <div>
-                    <button onClick={this.handleOnClick}>to homepage</button>
-                </div>
-
-                <Stage width={canvasWidth} height={canvasHeight} options={{ backgroundColor: 0xffffff }}>
-                    {
-                        initBlocks.map((block, i) => {
-                            const color = utils.rgb2hex([block.color.r, block.color.g, block.color.b])
-                            //console.log(block)
-                            return (
-                                <>
-                                    <Graphics
-                                        key={`${i}_graphic`}
-                                        draw={g => {
-                                            g.clear();
-                                            g.lineStyle(0)
-                                            g.beginFill(color, 1.0)
-                                            g.drawRect(block.x, canvasHeight - block.y - block.height, block.height, block.width)
-                                            g.endFill()
-                                        }}
-                                    />
-                                    <Text
-                                        key={`${i}_text`}
-                                        text={block.name}
-                                        anchor={0.5}
-                                        x={block.x + block.width/2}
-                                        y={canvasHeight - block.y - block.height/2}
-                                    />
-                                </>
-                            )
-                        })
-                    }
-                    {
-                        <Graphics
-                            key={`${0}_claw`}
-                            draw={g => {
-                                g.clear();
-                                g.lineStyle(4, 0x000000, 1)
-                                g.moveTo(claw[0].x, canvasHeight - claw[0].y + claw[0].height/2)
-                                g.lineTo(claw[0].x, canvasHeight - claw[0].y)
-                                g.lineTo(claw[0].x + claw[0].width, canvasHeight - claw[0].y)
-                                g.lineTo(claw[0].x + claw[0].width, canvasHeight - claw[0].y + claw[0].height/2)
-                                g.moveTo(claw[0].x + claw[0].width/2, canvasHeight - claw[0].y)
-                                g.lineTo(claw[0].x + claw[0].width/2, canvasHeight - claw[0].y - claw[0].height/2)
-                                g.moveTo(0, canvasHeight)
-                                g.lineTo(canvasWidth, canvasHeight)
-                            }}
-                        />
-                    }
-
-                </Stage>
-            </div>
-          );
+    handleSwitchStage(index) {
+        // Get Stage[index] blocks, and display
+        console.info("handleSwitchStage(), ", index);
+        // this.state.blockIndex = index;
+        this.setState({
+            blockIndex: index,
+            stepInfoIndex: index
+        });
     }
 
-  }
-  
-  export default PageFour;
+    render() {
+        return (
+            <div className={styles.container}>
+                <div className={styles.left}>
+                    <div className={styles.left_upper}>
+                        <div className={styles.sub_title}> Steps </div>
+                        {
+                            steps.map((step, i) => {
+                                return <div onClick={()=>{this.handleSwitchStage(i);}} key={i}><ul><li className={styles.stage_li}>{i + '. ' + step}</li></ul></div>;
+                            })
+                        }
+                    </div>
+                    <div>
+                        <div className={styles.sub_title}> Step Info </div>
+                        <div className={styles.step_info}>
+                        {
+                            stepInfo[this.state.stepInfoIndex]
+                        }
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.middle}>
+                    <div>
+                        <button onClick={this.handleOnClick}>to homepage</button>
+                    </div>
+                    <Stage width={canvasWidth_Middle} height={canvasHeight_Middle}
+                           options={{backgroundColor: 0xffffff}} key={'main-graph'}>
+                        {
+                            allBlocks[this.state.blockIndex].map((block, i) => {
+                                const color = utils.rgb2hex([block.color.r, block.color.g, block.color.b])
+                                //console.log(block)
+                                return (
+                                    <>
+                                        <Graphics
+                                            key={`${i}_graphic`}
+                                            draw={g => {
+                                                g.clear();
+                                                g.lineStyle(0)
+                                                g.beginFill(color, 1.0)
+                                                g.drawRect(block.x, canvasHeight_Middle - block.y - block.height, block.height, block.width)
+                                                g.endFill()
+                                            }}
+                                        />
+                                        <Text
+                                            key={`${i}_text`}
+                                            text={block.name}
+                                            anchor={0.5}
+                                            x={block.x + block.width / 2}
+                                            y={canvasHeight_Middle - block.y - block.height / 2}
+                                        />
+                                    </>
+                                )
+                            })
+                        }
+                        {
+                            <Graphics
+                                key={`${0}_claw`}
+                                draw={g => {
+                                    g.clear();
+                                    g.lineStyle(4, 0x000000, 1)
+                                    g.moveTo(claw[0].x, canvasHeight_Middle - claw[0].y + claw[0].height / 2 + 200)
+                                    g.lineTo(claw[0].x, canvasHeight_Middle - claw[0].y + 200)
+                                    g.lineTo(claw[0].x + claw[0].width, canvasHeight_Middle - claw[0].y + 200)
+                                    g.lineTo(claw[0].x + claw[0].width, canvasHeight_Middle - claw[0].y + claw[0].height / 2 + 200)
+                                    g.moveTo(claw[0].x + claw[0].width / 2, canvasHeight_Middle - claw[0].y + 200)
+                                    g.lineTo(claw[0].x + claw[0].width / 2, canvasHeight_Middle - claw[0].y - claw[0].height / 2 + 200)
+                                    g.moveTo(0, canvasHeight_Middle)
+                                    g.lineTo(canvasWidth_Middle, canvasHeight_Middle)
+                                }}
+                            />
+                        }
+
+                    </Stage>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.sub_title}>Subgoal</div>
+                    <div className={styles.sub_list}>
+                        {
+                            [...subGoal.keys()].map(key => {
+                                return <div className={styles.sub_item}>{key}</div>;
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+    );
+    }
+
+}
+
+export default PageFour;
 // export default function () {
 //     const [x, setX] = React.useState(0);
 //     const mask = React.useRef();
