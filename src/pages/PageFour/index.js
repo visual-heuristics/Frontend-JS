13864865d14d9d1d6a82fec4aaf2656415c47e5b
mezm94 <69788596+mezm94@users.noreply.github.com>
2021-10-01 20:04:07 +0800

    Draw sprites based on image in VFG file

--- src/pages/PageFour/index.js
+++ src/pages/PageFour/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {Stage, Text, Graphics, Sprite} from '@inlet/react-pixi';
+import {Stage, Text, Sprite} from '@inlet/react-pixi';
 import {utils} from 'pixi.js';
-import {subGoal, stepInfo, allBlocks, claw, steps, stepSubgoalMap, vfg} from './dataUtils';
+import {subGoal, stepInfo, allStages, steps, stepSubgoalMap, vfg} from './dataUtils';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
@@ -12,6 +12,7 @@ import ReplayIcon from '@material-ui/icons/Replay';
 import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
 import styles from './index.less';
+import * as PIXI from 'pixi.js'
 
 const canvasWidth_Middle = 800
 const canvasHeight_Middle = 480
@@ -43,7 +44,7 @@ class PageFour extends React.Component {
             showKey: '',
             showPlayButton: true,
             selectedSubGoals: {},
-            drawBlocks: allBlocks[0],
+            drawBlocks: allStages[0],
             playSpeed: 3,
             playButtonColor: 'primary',
             pauseButtonColor: 'default'
@@ -86,9 +87,9 @@ class PageFour extends React.Component {
 
     diff(index) {
         // 2 sets of blocks
-        const previousBlockIndex = this.state.blockIndex;
-        const previousBlocks = allBlocks[previousBlockIndex];
-        const newBlocks = allBlocks[index];
+        const previousStageIndex = this.state.blockIndex;
+        const previousStage = allStages[previousStageIndex];
+        const newStage = allStages[index];
 
         // Differ 2 sets of blocks
         // - find out, different blocks;
@@ -102,15 +103,15 @@ class PageFour extends React.Component {
         // ]
         // => movedBlocks
         const movedBlocks = {};
-        previousBlocks.map((eachBlock, i) => {
-            if(eachBlock.x !== newBlocks[i].x || eachBlock.y !== newBlocks[i].y) {
+        previousStage.map((eachBlock, i) => {
+            if(eachBlock.x !== newStage[i].x || eachBlock.y !== newStage[i].y) {
                 // 10
                 // { blockId => [{x:x1,y:y1}, {x: x2, y: y2}, , x3, .... x10] }
                 const changingPos = [];
                 for (let j = 0; j < 50; j++) {
                     const specificPos = {}
-                    specificPos.x = eachBlock.x + (newBlocks[i].x - eachBlock.x)/50 * (j + 1)
-                    specificPos.y = eachBlock.y + (newBlocks[i].y - eachBlock.y)/50 * (j + 1)
+                    specificPos.x = eachBlock.x + (newStage[i].x - eachBlock.x)/50 * (j + 1)
+                    specificPos.y = eachBlock.y + (newStage[i].y - eachBlock.y)/50 * (j + 1)
                     changingPos.push(specificPos)
                 }
                 movedBlocks[eachBlock.name] = changingPos
@@ -123,7 +124,7 @@ class PageFour extends React.Component {
             clearInterval(this.handler);
         }
         const handler = setInterval(()=>{
-            const newDrawBlocks = previousBlocks.map( block => {
+            const newDrawBlocks = previousStage.map( block => {
                 // block -> block.id
                 if(movedBlocks[block.name]) {
                     const move = movedBlocks[block.name];
@@ -335,9 +336,11 @@ class PageFour extends React.Component {
 
 
     render() {
-        // drawBlocks
-        const drawBlocks = this.state.drawBlocks;
-
+        // Get all sprites
+        var sprites = vfg.visualStages[this.state.stepInfoIndex].visualSprites;
+        // Sort sprites by their depth
+        sprites.sort((itemA, itemB) => itemA.depth - itemB.depth)
+    
         return (
             <div className={styles.container}>
                 <div className={styles.left}>
@@ -368,69 +371,76 @@ class PageFour extends React.Component {
                     <Stage width={canvasWidth_Middle} height={canvasHeight_Middle}
                            options={{backgroundColor: 0xffffff}} key={'main-graph'}>
                         {
-                            drawBlocks.map((block, i) => {
-                                // if()
-                                // currentBlockPos[block.name] = [block.x/2, block.y/2]
-                                const color = utils.rgb2hex([block.color.r, block.color.g, block.color.b])
-                                //console.log(block)
-                                return (
-                                    <>
-                                        <Graphics
-                                            // draw the block
-                                            key={`${i}_graphic`}
-                                            draw={g => {
-                                                g.clear();
-                                                g.lineStyle(0)
-                                                g.beginFill(color, 1.0)
-                                                g.drawRect(block.x/2, canvasHeight_Middle - block.y/2 - block.height/2, block.height/2, block.width/2)
-                                                g.endFill()
-                                            }}
-                                        />
-                                        <Text
-                                            // text on the block
-                                            key={`${i}_text`}
-                                            text={block.name}
-                                            anchor={0.5}
-                                            x={block.x/2 + block.width/4}
-                                            y={canvasHeight_Middle - block.y/2 - block.height/4}
-                                        />
-                                    </>
-                                )
+                            
+                            sprites.map((sprite, i) => {
+                                // Get the texture from base64 image storing in the vfg file
+                                var textureName = sprite.prefabimage
+                                var base = new PIXI.BaseTexture("data:image/png;base64,"+vfg.imageTable.m_values[vfg.imageTable.m_keys.indexOf(textureName)]);
+                                var texture = new PIXI.Texture(base);
+                                // Get the color of the sprite
+                                var color = utils.rgb2hex([sprite.color.r, sprite.color.g, sprite.color.b])
+                                // Initialize the rotation of the sprite
+                                var rotation = 0
+                                // Initialize the x-axis coordinate position of the sprite
+                                var x = sprite.minX * canvasHeight_Middle
+                                // Initialize the y-axis coordinate position of the sprite
+                                var y = canvasHeight_Middle - sprite.maxY * canvasHeight_Middle
+                                // Initialize the anchor (i.e. the origin point) of the sprite
+                                var anchor = (0, 0)
+                                // Update the anchor, rotation, (x,y) location if the sprite need to be rotated
+                                if ('rotate' in sprite){
+                                    anchor = (0.5, 0.5)
+                                    rotation = sprite.rotate * Math.PI / 180;
+                                    x = sprite.minX * canvasHeight_Middle + (sprite.maxX - sprite.minX) * canvasHeight_Middle/2
+                                    y = canvasHeight_Middle - sprite.minY * canvasHeight_Middle
+                                }
+                                // Draw the sprite with a text
+                                if (sprite.showname) {
+                                    return (
+                                        <>
+                                            <Sprite
+                                                texture = {texture}
+                                                name = {sprite.name}
+                                                anchor = {anchor}
+                                                rotation = {rotation}
+                                                x = {x}
+                                                y = {y}
+                                                width = {(sprite.maxX - sprite.minX) * canvasHeight_Middle}
+                                                height = {(sprite.maxY - sprite.minY) * canvasHeight_Middle}
+                                                tint = {color}
+                                            />
+                                            <Text
+                                                // text on the sprite
+                                                text = {sprite.name}
+                                                style = {{fontFamily: 'Arial', fontSize: 16, fill: 0x000000}}
+                                                anchor = {(0.5, 0.5)}
+                                                x = {x + (sprite.maxX - sprite.minX) * canvasHeight_Middle / 2}
+                                                y = {y + (sprite.maxY - sprite.minY) * canvasHeight_Middle / 2}
+                                            />
+                                        </>
+                                    )
+                                }
+                                // Draw the sprite without text
+                                else{
+                                    return (
+                                        <>
+                                            <Sprite
+                                                texture = {texture}
+                                                name = {sprite.name}
+                                                anchor={anchor}
+                                                rotation = {rotation}
+                                                x = {x}
+                                                y = {y}
+                                                width = {(sprite.maxX - sprite.minX) * canvasHeight_Middle}
+                                                height = {(sprite.maxY - sprite.minY) * canvasHeight_Middle}
+                                                tint = {color}
+                                            />
+                                        </>
+                                    )
+                                } 
                             })
                         }
-                        {
-                            // Draw the claw
-
-                            //reading directly from vfg, but in a (future) generalisable way, we don't know if there are claws or blocks
-                            //we just render them all, which is easier
-
-                            <Sprite
-                            image={"data:image/png;base64,"+vfg.imageTable.m_values[vfg.imageTable.m_keys.indexOf('img-claw')]} 
-                            scale={{ x: 0.5, y: 0.5 }}
-                            anchor={(0,0)}
-                            x={canvasWidth_Middle/4-50} // this must be extracted from vfg, this is just example
-                            
-                        
-                            />
-                            /*<Graphics
-                                key={`${0}_claw`}
-                                draw={g => {
-                                    g.clear();
-                                    g.lineStyle(2, 0x000000, 1)
-                                    g.moveTo(claw[0].x/2, canvasHeight_Middle - claw[0].y/2 + claw[0].height/4 -10)
-                                    g.lineTo(claw[0].x/2, canvasHeight_Middle - claw[0].y/2 -10)
-                                    g.lineTo(claw[0].x/2 + claw[0].width/2, canvasHeight_Middle - claw[0].y/2 -10)
-                                    g.lineTo(claw[0].x/2 + claw[0].width/2, canvasHeight_Middle - claw[0].y/2 + claw[0].height/4 -10)
-                                    g.moveTo(claw[0].x/2 + claw[0].width/2 - 20, canvasHeight_Middle - claw[0].y/2 -10)
-                                    g.lineTo(claw[0].x/2 + claw[0].width/2 - 20, canvasHeight_Middle - claw[0].y/2 - claw[0].height/4 -10)
-                                    g.moveTo(0, canvasHeight_Middle)
-                                    g.lineTo(canvasWidth_Middle, canvasHeight_Middle)
-                                }}
-                            />
-                            */
-                        }
                     </Stage>
-                    {/*controller buttons*/}
                     <div style={{height:'50px'}}>
                         <IconButton color="primary" style={{float:'left', marginLeft:'6%', marginRight:'5%'}} onClick={()=>{this.handlePreviousClick(this.state.stepInfoIndex);}}>
                             <SkipPreviousIcon fontSize="large" />
